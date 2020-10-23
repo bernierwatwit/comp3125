5dc829577565da799eb0f156da175b580399ad26
bernierwatwit <55259225+bernierwatwit@users.noreply.github.com>
2020-10-23 13:38:13 -0400

    Add files via upload

---
+++ pong_walls.py
@@ -0,0 +1,39 @@
+import pygame
+
+pygame.init()
+pygame.display.set_caption('my pong')
+
+WIDTH = 800
+HEIGHT = 400
+#surface
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen.fill((0,0,0))
+#double buffering: stage updates together; update them at once
+#avoids flickering
+pygame.display.update()
+
+#walls
+# Rect(surface, color, rect) -> Rect
+# Rect((left, top), (width, height)) -> Rect
+wcolor = pygame.Color('blue')
+BORDER = 15
+#top wall
+pygame.draw.rect(screen, wcolor, pygame.Rect((0, 0), (WIDTH, BORDER)))
+#left wall
+pygame.draw.rect(screen, wcolor, pygame.Rect((0, 0), (BORDER, HEIGHT)))
+#bottom wall
+pygame.draw.rect(screen, wcolor, pygame.Rect((0, HEIGHT-BORDER), (WIDTH, BORDER)))
+
+pygame.display.update()
+# define a variable to control the main loop
+running = True
+    
+# main loop
+while running:
+    # event handling, gets all event from the event queue
+    for event in pygame.event.get():
+        # only do something if the event is of type QUIT
+        if event.type == pygame.QUIT:
+            # change the value to False, to exit the main loop
+            running = False
+#done
